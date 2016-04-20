ddb1662f49a470eea98f1f0534f1d2e6e21a7d1b
jtam11 <justinktam@gmail.com>
2016-04-20 00:35:59 -0700

    Using directives to show cards and scores

--- starter-code/app/js/app.js
+++ starter-code/app/js/app.js
@@ -1,8 +1,37 @@
 angular
   .module('CardsAgainstAssembly', [])
   .controller('CardsController', CardsController)
-  .controller('PlayersController', PlayersController);
+  .controller('PlayersController', PlayersController)
+  .directive('card', CardViewDirective)
+  .directive('scores', ScoreViewDirective);
 
+function CardViewDirective() {
+  var directive = {
+    restrict: 'E',
+    replace: false,
+    templateUrl: './templates/_cardView.html',
+    scope: {
+      question: '@'
+    }
+
+  };
+  return directive;
+}
+
+function ScoreViewDirective() {
+  var directive = {
+    restrict: 'E',
+    replace: false,
+    templateUrl: './templates/_scoreView.html',
+    scope: {
+      name: '@',
+      points: '@'
+    }
+  };
+  return directive;
+}
+
+// controllers start here
 function CardsController() {
   var vm = this;
   vm.questions = [
@@ -11,7 +40,8 @@ function CardsController() {
     {question: "I get by with a little help from _________."},
     {question: "_________. It's a trap!"},
     {question: "The class field trip was completely ruined by _________."},
-    {question: "What's my secret power?"}
+    {question: "What's my secret power?"},
+    {question: "My finger smells like ___________."}
   ];
 }
 
